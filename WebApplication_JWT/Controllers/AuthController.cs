5ed072f9ca3d7461f4f66da656509df097ee4660
Wilson Ren <waston@fakeemail.com>
2023-04-23 23:37:00 +0800

    register

---
+++ WebApplication_JWT/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication_JWT.Models;
+
+namespace WebApplication_JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request) 
+        {
+            string passwordHash =BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.UserName;
+            user.PasswordHash = passwordHash;
+            return Ok(user);
+        }
+    }
+}
